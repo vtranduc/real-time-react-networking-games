69b10b4881b24da7c2801756e20e0609aa0e7619
JayJayTing <tingjayj@gmail.com>
2019-08-29 22:28:21 +0800

    fixed minor bugs

--- src/components/soccer/Soccer.js
+++ src/components/soccer/Soccer.js
@@ -3,95 +3,106 @@ import useKeyPress from "../../helpers/useKeyPress";
 // const useKeyPress = require("../../helpers/useKeyPress");
 
 export default function Soccer({ socket }) {
-  //----------------------Literals
-  const temporaryRoom = "testingSoccer";
-  // All values for this myst be integers
-  const fieldSpec = { height: 600, width: 800, top: 150, left: 50 };
-  // Both width and height are the ratio relative to the fieldSpec's width
-  // The must be the value between 0 and 1
-  const ballSpec = { width: 0.05, height: 0.05 };
-  const playerSpec = { width: 0.1, height: 0.1 };
-  const config = { frameDuration: 0.1 };
-  //----------------------
+	//----------------------Literals
+	const temporaryRoom = "testingSoccer";
+	// All values for this myst be integers
+	const fieldSpec = { height: 600, width: 800, top: 150, left: 50 };
+	// Both width and height are the ratio relative to the fieldSpec's width
+	// The must be the value between 0 and 1
+	const ballSpec = { width: 0.05, height: 0.05 };
+	const playerSpec = { width: 0.1, height: 0.1 };
+	const config = { frameDuration: 0.1 };
+	const playerPhysics = {
+		accel: { x: 1.2, y: 1.2 },
+		reverseAccel: { x: 3, y: 3 },
+		resistance: { x: 1.2, y: 1.2 },
+		wallBounce: 0.8
+	};
+	//----------------------
 
-  const [spritePos, setSpritePos] = useState(null);
+	const [spritePos, setSpritePos] = useState(null);
 
-  useEffect(() => {
-    socket.emit("soccerInit", { room: temporaryRoom, fieldSpec, config });
-    socket.on("soccerUpdateGame", data => {
-      console.log("update the game");
-      console.log(data);
-      setSpritePos(data);
-    });
-    return () => {
-      console.log("Unmounting!");
-      socket.removeListener("soccerUpdateGame");
+	useEffect(() => {
+		socket.emit("soccerInit", {
+			room: temporaryRoom,
+			fieldSpec,
+			config,
+			playerPhysics
+		});
+		socket.on("soccerUpdateGame", data => {
+			console.log("update the game");
+			console.log(data);
+			setSpritePos(data);
+		});
+		return () => {
+			console.log("Unmounting!");
+			socket.removeListener("soccerUpdateGame");
 
-      socket.emit("soccerDisconnect", temporaryRoom);
-    };
-  }, []);
+			socket.emit("soccerDisconnect", temporaryRoom);
+		};
+	}, []);
 
-  const w = useKeyPress("w");
-  const a = useKeyPress("a");
-  const d = useKeyPress("d");
-  const s = useKeyPress("s");
+	const w = useKeyPress("w");
+	const a = useKeyPress("a");
+	const d = useKeyPress("d");
+	const s = useKeyPress("s");
 
-  useEffect(() => {
-    socket.emit("soccerHandleKeyPress", {
-      axis: "x",
-      dir: a ? (d ? "" : "left") : d ? "right" : "",
-      room: temporaryRoom
-    });
-  }, [a, d]);
-  useEffect(() => {
-    socket.emit("soccerHandleKeyPress", {
-      axis: "y",
-      dir: w ? (s ? "" : "up") : s ? "down" : "",
-      room: temporaryRoom
-    });
-  }, [w, s]);
+	useEffect(() => {
+		socket.emit("soccerHandleKeyPress", {
+			axis: "x",
+			dir: a ? (d ? "" : "left") : d ? "right" : "",
+			room: temporaryRoom
+		});
+	}, [a, d]);
+	useEffect(() => {
+		socket.emit("soccerHandleKeyPress", {
+			axis: "y",
+			dir: w ? (s ? "" : "up") : s ? "down" : "",
+			room: temporaryRoom
+		});
+	}, [w, s]);
 
-  return (
-    <>
-      <img
-        src="assets/soccer/field.jpg"
-        style={{
-          height: fieldSpec.height,
-          width: fieldSpec.width,
-          position: "absolute",
-          top: fieldSpec.top,
-          left: fieldSpec.left
-        }}
-      ></img>
-      {spritePos && (
-        <div>
-          <img
-            src="assets/soccer/ball.png"
-            style={{
-              height: Math.floor(ballSpec.height * fieldSpec.width),
-              width: Math.floor(ballSpec.width * fieldSpec.width),
-              position: "absolute",
-              top: spritePos.ball.y,
-              left: spritePos.ball.x
-            }}
-          ></img>
-          {Object.keys(spritePos.players).map(socketId => {
-            return (
-              <img
-                key={socketId}
-                src="assets/soccer/player.png"
-                style={{
-                  height: Math.floor(playerSpec.height * fieldSpec.width),
-                  width: Math.floor(playerSpec.width * fieldSpec.width),
-                  top: spritePos.players[socketId].y,
-                  left: spritePos.players[socketId].x,
-                  position: "absolute"
-                }}
-              ></img>
-            );
-          })}
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<img
+				src="assets/soccer/field.jpg"
+				style={{
+					height: fieldSpec.height,
+					width: fieldSpec.width,
+					position: "absolute",
+					top: fieldSpec.top,
+					left: fieldSpec.left
+				}}
+			></img>
+			{spritePos && (
+				<div>
+					<img
+						src="assets/soccer/ball.png"
+						style={{
+							height: Math.floor(ballSpec.height * fieldSpec.width),
+							width: Math.floor(ballSpec.width * fieldSpec.width),
+							position: "absolute",
+							top: spritePos.ball.y,
+							left: spritePos.ball.x
+						}}
+					></img>
+					{Object.keys(spritePos.players).map(socketId => {
+						return (
+							<img
+								key={socketId}
+								src="assets/soccer/player.png"
+								style={{
+									height: Math.floor(playerSpec.height * fieldSpec.width),
+									width: Math.floor(playerSpec.width * fieldSpec.width),
+									top: spritePos.players[socketId].y,
+									left: spritePos.players[socketId].x,
+									position: "absolute"
+								}}
+							></img>
+						);
+					})}
+				</div>
+			)}
+		</>
+	);
 }
--- src/server/soccer/soccerServer.js
+++ src/server/soccer/soccerServer.js
@@ -1,106 +1,130 @@
 let getRandomInt = require("../../helpers/getRandomInt");
 
 const soccerGame = function(socket, sockets, rooms, soccerData) {
-  console.log("this is a soccer game");
-  socket.on("soccerHandleKeyPress", data => {
-    // console.log("data has been received: ", data);
-    soccerData[data.room].players[socket.id].commands[data.axis] = data.dir;
-    console.log(
-      "current command is: ",
-      soccerData[data.room].players[socket.id].commands
-    );
-  });
-  socket.on("soccerInit", data => {
-    console.log("A user has joined the room: ", socket.id);
-    socket.join(data.room);
-    if (!soccerData[data.room]) {
-      soccerData[data.room] = {
-        config: {},
-        players: {},
-        interval: null,
-        ball: {
-          pos: {
-            x: getRandomInt(
-              data.fieldSpec.left,
-              data.fieldSpec.left + data.fieldSpec.width
-            ),
-            y: data.fieldSpec.top
-          }
-        }
-      };
-      //----Start the game-------
-      soccerData[data.room].interval = setInterval(() => {
-        updateSoccerGame(soccerData[data.room]);
-        console.log("updating");
-        sockets
-          .to(data.room)
-          .emit("soccerUpdateGame", allPos(soccerData, data.room));
-      }, data.config.frameDuration * 1000);
-      //---------------------------
-    }
+	console.log("this is a soccer game");
+	socket.on("soccerHandleKeyPress", data => {
+		// console.log("data has been received: ", data);
+		try {
+			soccerData[data.room].players[socket.id].commands[data.axis] = data.dir;
+			console.log(
+				"current command is: ",
+				soccerData[data.room].players[socket.id].commands
+			);
+		} catch (error) {
+			console.log("Room is not ready!");
+		}
+	});
+	socket.on("soccerInit", data => {
+		console.log("A user has joined the room: ", socket.id);
+		socket.join(data.room);
+		if (!soccerData[data.room]) {
+			soccerData[data.room] = {
+				config: {
+					frameDuration: data.fieldSpec.frameDuration,
+					playerPhysics: data.playerPhysics
+				},
+				players: {},
+				interval: null,
+				ball: {
+					pos: {
+						x: getRandomInt(
+							data.fieldSpec.left,
+							data.fieldSpec.left + data.fieldSpec.width
+						),
+						y: data.fieldSpec.top
+					}
+				}
+			};
+			//----Start the game-------
+			soccerData[data.room].interval = setInterval(() => {
+				updateSoccerGame(soccerData[data.room]);
+				// console.log("updating");
+				sockets
+					.to(data.room)
+					.emit("soccerUpdateGame", allPos(soccerData, data.room));
+			}, data.config.frameDuration * 1000);
+			//---------------------------
+		}
 
-    soccerData[data.room].players[socket.id] = {
-      pos: {
-        x: getRandomInt(
-          data.fieldSpec.left,
-          data.fieldSpec.left + data.fieldSpec.width
-        ),
-        y: data.fieldSpec.top
-      },
-      commands: { x: "", y: "" }
-    };
-  });
+		soccerData[data.room].players[socket.id] = {
+			pos: {
+				x: getRandomInt(
+					data.fieldSpec.left,
+					data.fieldSpec.left + data.fieldSpec.width
+				),
+				y: data.fieldSpec.top
+			},
+			commands: { x: "", y: "" }
+		};
+	});
 
-  const updateSoccerGame = function(roomData) {
-    for (let socketId in roomData.players) {
-      if (roomData.players[socketId].commands.x === "right") {
-        roomData.players[socketId].pos.x += 5;
-      }
-    }
-  };
+	// ==============ALL THE DISCONNECTIONS==============================
 
-  // ==============ALL THE DISCONNECTIONS==============================
+	socket.on("soccerDisconnect", room => {
+		// if (soccerData[room] && soccerData[room].players[socket.id]) {
+		socket.leave(room);
+		delete soccerData[room].players[socket.id];
+		console.log("deleted: " + socket.id + ": ", soccerData);
+		console.log("Remaining users: ", Object.keys(soccerData[room].players));
+		if (Object.keys(soccerData[room].players).length === 0) {
+			clearInterval(soccerData[room].interval);
+			delete soccerData[room];
+			console.log("Deleted the entire room!");
+		}
+		// }
+	});
 
-  socket.on("soccerDisconnect", room => {
-    // if (soccerData[room] && soccerData[room].players[socket.id]) {
-    socket.leave(room);
-    delete soccerData[room].players[socket.id];
-    console.log("deleted: " + socket.id + ": ", soccerData);
-    console.log("Remaining users: ", Object.keys(soccerData[room].players));
-    if (Object.keys(soccerData[room].players).length === 0) {
-      clearInterval(soccerData[room].interval);
-      delete soccerData[room];
-      console.log("Deleted the entire room!");
-    }
-    // }
-  });
-
-  socket.on("disconnect", () => {
-    //----------------------Literals
-    const room = "testingSoccer";
-    //----------------------
-    if (soccerData[room] && soccerData[room].players[socket.id]) {
-      delete soccerData[room].players[socket.id];
-      console.log("deleted: " + socket.id + ": ", soccerData);
-      console.log("Remaining users: ", Object.keys(soccerData[room].players));
-      if (Object.keys(soccerData[room].players).length === 0) {
-        clearInterval(soccerData[room].interval);
-        delete soccerData[room];
-        console.log("Deleted the entire room!");
-      }
-    }
-  });
+	socket.on("disconnect", () => {
+		//----------------------Literals
+		const room = "testingSoccer";
+		//----------------------
+		if (soccerData[room] && soccerData[room].players[socket.id]) {
+			delete soccerData[room].players[socket.id];
+			console.log("deleted: " + socket.id + ": ", soccerData);
+			console.log("Remaining users: ", Object.keys(soccerData[room].players));
+			if (Object.keys(soccerData[room].players).length === 0) {
+				clearInterval(soccerData[room].interval);
+				delete soccerData[room];
+				console.log("Deleted the entire room!");
+			}
+		}
+	});
 };
+
+const updateSoccerGame = function(roomData) {
+	for (let socketId in roomData.players) {
+		if (roomData.players[socketId].commands.x === "right") {
+			roomData.players[socketId].pos.x += 5;
+			console.log("yooooo");
+		} else if (roomData.players[socketId].commands.x === "left") {
+			roomData.players[socketId].pos.x -= 5;
+		} else if (roomData.players[socketId].commands.x === "") {
+			// roomData.players[socketId].pos.x += 5;
+		}
+		if (roomData.players[socketId].commands.y === "up") {
+			roomData.players[socketId].pos.y -= 5;
+		} else if (roomData.players[socketId].commands.y === "down") {
+			roomData.players[socketId].pos.y += 5;
+		} else if (roomData.players[socketId].commands.y === "") {
+			// roomData.players[socketId].pos.x += 5;
+		}
+	}
+};
+
+const displacement = function(vi, accel, time) {
+	return vi * time + 0.5 * accel * time * time;
+};
+
 //tells the clients where all the players are.
 const allPos = function(soccerData, room) {
-  let output = {
-    ball: soccerData[room].ball.pos,
-    players: {}
-  };
-  for (let socketId in soccerData[room].players) {
-    output.players[socketId] = soccerData[room].players[socketId].pos;
-  }
-  return output;
+	let output = {
+		ball: soccerData[room].ball.pos,
+		players: {}
+	};
+	for (let socketId in soccerData[room].players) {
+		output.players[socketId] = soccerData[room].players[socketId].pos;
+	}
+	return output;
 };
 
 module.exports = soccerGame;
