399c4a77a9ea89c69c8ff7dd242e30f6d6e764d2
JayJayTing <tingjayj@gmail.com>
2019-09-02 00:16:32 -0400

    created tables

---
+++ db/schema/user_id.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user_id {
+    id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL, 
+    room_id INTEGER REFERENCES room(id) ON DELETE CASCADE,
+    game_id INTEGER REFERENCES game(id) ON DELETE CASCADE 
+}
+
+
+
