b6d37eb285925c7ee218437d22422a0cc67fb28b
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-09-03 19:00:21 +0000

    updated lobby

--- src/server/lobby/index.js
+++ src/server/lobby/index.js
@@ -1,21 +1,22 @@
+const getRooms = require("../helpers/getRooms");
+
 const lobby = function(socket, sockets, rooms, gameData, io) {
   console.log("initializing lobby");
 
   socket.on("lobbyConnect", () => {
-    console.log("whatever you get");
     socket.join("lobby");
-    sockets.to("lobby").emit("lobbySetup", {
-      soccer: Object.keys(gameData.soccer),
-      eggCatch: Object.keys(gameData.eggCatch),
-      world: Object.keys(gameData.world)
-    });
+    sockets.to("lobby").emit("lobbyUpdate", getRooms(gameData));
   });
 
+  //========================================
   socket.on("lobbyDisconnect", () => {
     console.log("leaving lobby", socket.id);
     socket.leave("lobby");
   });
-  socket.on("disconnect", () => {});
+  socket.on("disconnect", () => {
+    console.log("A client has left the lobby: ", socket.id);
+  });
+  //=========================================
 };
 
 module.exports = lobby;
