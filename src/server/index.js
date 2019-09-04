f7cecd1619713fe5766247cec3dff6079934feb7
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-09-04 08:36:54 +0000

    updated lobby

--- src/server/index.js
+++ src/server/index.js
@@ -105,7 +105,7 @@ const gameData = {
   soccer: {
     lobby: {
       soccerRoom1: {
-        status: "open",
+        status: "",
         players: {},
         chats: [
           { key: ";czvxzc", user: "Good duke", msg: "Hello all" },
@@ -114,8 +114,8 @@ const gameData = {
         ]
       },
       soccerRoom2: {
-        status: "open",
-        players: { jayjay: {}, sarah: {} },
+        status: "aaa",
+        players: { jayjay: { ready: false }, sarah: { ready: true } },
         chats: [
           { key: ";pfdzzzzzzzzsAsaj", user: "Jayjay", msg: "I wanna eat" },
           { key: ";pfdsbbaj", user: "Sarah", msg: "Ok!" }
