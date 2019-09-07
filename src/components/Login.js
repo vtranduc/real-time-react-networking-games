6d1965855073e356763f72806b337f73932036f4
JayJayTing <tingjayj@gmail.com>
2019-09-06 21:57:24 -0400

    started about page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../styles/login.css";
 import axios from "axios";
+import Cookies from "universal-cookie";
 
 function Login({ loginStatus, setLoginStatus, setProfileInfo }) {
 	const [userInfo, setUserInfo] = useState({ email: "", password: "" });
@@ -9,7 +10,7 @@ function Login({ loginStatus, setLoginStatus, setProfileInfo }) {
 		// console.log("submit handled");
 		event.preventDefault();
 		//alert(`${user.username} ${user.password}`);
-
+		let cookies = new Cookies();
 		//--------------this is to login, maybe need to add useeffect-----------
 		axios
 			.post("http://localhost:3001/login", {
@@ -19,13 +20,29 @@ function Login({ loginStatus, setLoginStatus, setProfileInfo }) {
 			.then(response => {
 				if (response.data.length) {
 					setLoginStatus(true);
+					console.log("cookies should be setting now");
 					//this should be set to a cookies session instead
+
+					// console.log(
+					// 	JSON.stringify({
+					// 		username: response.data[0].username,
+					// 		avatar: response.data[0].avatar,
+					// 		firstName: response.data[0].first_name,
+					// 		lastName: response.data[0].last_name
+					// 	})
+					// );
+					cookies.set("profile", response.data[0].username, {
+						path: "http://localhost:3001/login"
+					});
 					setProfileInfo({
 						username: response.data[0].username,
 						avatar: response.data[0].avatar,
 						firstName: response.data[0].first_name,
 						lastName: response.data[0].last_name
 					});
+
+					//
+
 					console.log(loginStatus);
 					console.log("this is right");
 				}
