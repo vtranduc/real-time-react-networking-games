1213a306b1085e2e3b872a848a983becc0b2854a
vtranduc <vtranduc@ryerson.ca>
2019-09-08 01:52:18 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -15,15 +15,10 @@ import Profile from "./components/Profile";
 import PhaserGame from "./components/PhaserGame";
 import RockPaperScissors from "./components/rockPaperScissors/RockPaperScissors";
 import axios from "axios";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import io from "socket.io-client";
 import Cookies from "universal-cookie";
 
-// import { makeStyles } from "@material-ui/core/styles";
-// import Button from "@material-ui/core/Button";
-// import { Socket } from "net";
 const serverPORT = 3001;
 
 function App() {
@@ -33,23 +28,16 @@ function App() {
   const [profileInfo, setProfileInfo] = useState(null);
   const httpServer = "http://localhost:3001/";
   //-------------------------------------------------------
-  // useEffect(() => {
-  //   console.log(profileInfo);
-  // }, [profileInfo]);
   let socket = io(`:${serverPORT}`);
   console.log("initializing app");
 
   useEffect(() => {
-    // console.log("load the cookie when component loads");
-
     let cookies = new Cookies();
     if (cookies.get("profile")) {
       console.log("LOGGED IN HERE BABY!");
       axios
         .post(`${httpServer}loggedInStatus`, { cookie: cookies.get("profile") })
         .then(response => {
-          console.log("bring back pull");
-          console.log(response.data);
           if (response.data.length === 1) {
             setLoginStatus(true);
             setProfileInfo({
@@ -62,12 +50,6 @@ function App() {
               username: response.data[0].username,
               avatar: response.data[0].avatar
             });
-            // console.log("SETTING TO THIS", {
-            //   username: response.data[0].username,
-            //   avatar: response.data[0].avatar,
-            //   firstName: response.data[0].first_name,
-            //   lastName: response.data[0].last_name
-            // });
           } else {
             console.log("did not find the user!");
             cookies.remove("profile");
@@ -121,26 +103,11 @@ function App() {
                     socket={socket}
                     setRoom={setRoom}
                     profileInfo={profileInfo}
-                    // profileInfo={{
-                    //   username: "Maki",
-                    //   avatar:
-                    //     "https://avatarfiles.alphacoders.com/148/148267.png"
-                    // }}
                   />
                 ) : (
                   <h3>Retrieving user info...</h3>
                 )}
               </div>
-
-              // {profileInfo ? <Lobby
-              //   socket={socket}
-              //   setRoom={setRoom}
-              //   // profileInfo={profileInfo}
-              //   profileInfo={{
-              //     username: "Maki",
-              //     avatar: "https://avatarfiles.alphacoders.com/148/148267.png"
-              //   }}
-              // />: <h3>Retrieving</h3>}
             );
           }}
         />
@@ -175,7 +142,6 @@ function App() {
             return <Register httpServer={httpServer} />;
           }}
         />
-
         <Route
           path="/soccer"
           exact
@@ -208,72 +174,6 @@ function App() {
       </Switch>
     </Router>
   );
-  //       <Route path="/register" exact component={Register} />
-  //       <Route path="/world" exact component={World} />
-  //       <Route
-  //         path="/soccer"
-  //         exact
-  //         render={() => {
-  //           return <Soccer socket={socket} room={room} />;
-  //         }}
-  //       />
-  //       <Route
-  //         path="/rockpaperscissors"
-  //         exact
-  //         render={() => {
-  //           return <RockPaperScissors socket={socket} />;
-  //         }}
-  //       />
-  //       <Route
-  //         path="/chansey"
-  //         exact
-  //         render={() => {
-  //           return <EggCatchGame socket={socket} />;
-  //         }}
-  //       />
-  //       <Route path="/phaser-game" exact component={PhaserGame} />
-  //       <Route path="/profile" exact component={Profile} />
-  //     </Switch>
-  //   </Router>
-  // );
-
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
