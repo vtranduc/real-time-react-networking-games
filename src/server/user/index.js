606025792e102d92cde2dbce0177eb54b4bb312f
vtranduc <vtranduc@ryerson.ca>
2019-09-08 12:35:07 -0400

    just small setup

--- src/server/user/index.js
+++ src/server/user/index.js
@@ -7,12 +7,12 @@ const {
 } = require("../../../db/queries/allQueries");
 
 const profileServerData = function(app, pool) {
-  console.log("hug hug mog mog sugar less");
-  app.get("/getmessages/:username", (req, res) => {
-    getMessage(req.params.username).then(data => {
-      res.send(data);
-    });
-  });
+  // console.log("hug hug mog mog sugar less");
+  // app.get("/getmessages/:username", (req, res) => {
+  //   getMessage(req.params.username).then(data => {
+  //     res.send(data);
+  //   });
+  // });
 
   app.post("/postmessage", (req, res) => {
     let sender = req.body.sender;
@@ -20,8 +20,11 @@ const profileServerData = function(app, pool) {
     let title = req.body.title;
     let message = req.body.message;
     console.log(sender, "jzizzless");
-
     postMessage(sender, reciever, title, message);
+  });
+
+  app.post("retrieveuserprofile", (req, res) => {
+    console.log("I am looking for: ", req.body);
   });
 };
 
